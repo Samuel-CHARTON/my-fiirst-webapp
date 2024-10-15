7bca21434c49abaf8f590d05fea53e87169ad72f
Samuel-CHARTON <samuel@emoticonnect.io>
2024-10-15 14:46:02 +0200

    added get user by email add user and remove user

--- database_manager.pm
+++ database_manager.pm
@@ -6,7 +6,7 @@ use JSON;
 use Exporter 'import';
 use Carp;
 
-our @EXPORT_OK = qw(get_users get_user_by_id);
+our @EXPORT_OK = qw(get_users get_user_by_id get_user_by_email add_user remove_user);
 
 my $users_file = 'fake_db.json';
 
@@ -16,6 +16,17 @@ sub get_users {
     my $json_text = <$fh>;
     close $fh;
     return decode_json($json_text);
+}
+
+sub get_last_id {
+    my $users = get_users();
+    my $max_id = 0;
+    foreach my $user (@$users) {
+        if ($user->{id} > $max_id) {
+            $max_id = $user->{id};
+        }
+    }
+    return $max_id;
 }
 
 sub get_user_by_id {
@@ -28,4 +39,49 @@ sub get_user_by_id {
     }
     return undef;
 }
+
+sub get_user_by_email {
+    my $email = shift;
+    my $users = get_users();
+    foreach my $user (@$users) {
+        if ($user->{email} eq $email) {
+            return $user;
+        }
+    }
+    return undef;
+}
+
+sub add_user {
+    # arg 1: name, arg 2: email
+    my $name = shift;
+    my $email = shift;
+    my $users = get_users();
+    my $new_id = get_last_id() + 1;
+    my $new_user = {
+        id => $new_id,
+        name => $name,
+        email => $email
+    };
+    push @$users, $new_user;
+    open my $fh, '>', $users_file or croak "Can't open $users_file: $!";
+    print $fh encode_json($users);
+    close $fh;
+    return $new_user;
+}
+
+sub remove_user {
+    my $id = shift;
+    my $users = get_users();
+    my $new_users = [];
+    foreach my $user (@$users) {
+        if ($user->{id} != $id) {
+            push @$new_users, $user;
+        }
+    }
+    open my $fh, '>', $users_file or croak "Can't open $users_file: $!";
+    print $fh encode_json($new_users);
+    close $fh;
+    return 1;
+}
+
 1;
--- test_database_manager.pl
+++ test_database_manager.pl
@@ -3,7 +3,7 @@ use lib './';
 use strict;
 use warnings;
 use Data::Dumper;
-use database_manager qw(get_users get_user_by_id);
+use database_manager qw(get_users get_user_by_id get_user_by_email add_user remove_user);
 
 # test get_users
 my $users = get_users();
@@ -14,3 +14,20 @@ print Dumper($users);
 my $user = get_user_by_id(1);
 print "User with id 1:\n";
 print Dumper($user);
+
+# test get_user_by_email
+$user = get_user_by_email('mec2@example.com');
+print "User with email mec2\@example.com:\n";
+print Dumper($user);
+
+# test add_user
+add_user('mec3', 'mec3@gmail.com');
+$users = get_users();
+print "Users after adding mec3:\n";
+print Dumper($users);
+
+# test remove_user
+remove_user(3);
+$users = get_users();
+print "Users after removing user with id 3:\n";
+print Dumper($users);
