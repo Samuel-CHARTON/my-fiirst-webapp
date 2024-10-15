17a5023733378ec28c9991aa08be1372a91a56c9
Samuel-CHARTON <samuel@emoticonnect.io>
2024-10-15 15:29:15 +0200

    small test with templates

--- myapp.pl
+++ myapp.pl
@@ -1,9 +1,24 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite -signatures;
 
+use lib './';
+use database_manager qw(get_users get_user_by_id get_user_by_email add_user remove_user);
+
 get '/' => sub ($c) {
   $c->render(template => 'index');
 };
+
+get '/list' => sub ($c) {
+  $c->stash(tab => generate_tab());
+  $c->render(template => 'list');
+};
+
+sub generate_tab {
+  my $users = get_users();
+  my $test = ["<div id='name'><p>oui</p></div>"];
+  push @$test, "<div id='name'><p>oui</p></div>";
+  return $test;
+}
 
 app->start;
 __DATA__
@@ -19,3 +34,9 @@ __DATA__
   <head><title><%= title %></title></head>
   <body><%= content %></body>
 </html>
+
+@@ list.html.ep
+<h1>List of Users<h1>
+<% foreach my $tabs (@$tab) { %>
+  <%== $tabs %>
+<% } %>
